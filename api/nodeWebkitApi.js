5b638077dd433fe7461b8141306fdc89a0aa85bd
Rick Rutt <RuttR@Valassis.com>
2015-04-02 16:05:21 -0400

    Experiment with node-webkit to create desktop application.

---
+++ api/nodeWebkitApi.js
@@ -0,0 +1,85 @@
+(function () {
+    'use strict';
+
+    var util = require('util');
+
+    var cbWrapper = require('./lib/couchbaseWrapper');
+
+    exports.invokeControllerAction = function (logger, argv, credentials, controllerName, actionName, params, callback) {
+        var _validate = function (controller, params, innerCallback) {
+            var validator = controller['validate'];
+            if (validator) {
+                validator(params, function (error) {
+                    return innerCallback(error);
+                });
+            }
+            else {
+                return innerCallback(null);
+            }
+        };
+
+        var _logAndSendErrorOrResult = function (error, result) {
+            var message = {
+                controllerName: controllerName,
+                actionName: actionName
+            };
+            if (error) {
+                message.error = error;
+                logger.error(message);
+                return callback(message);
+            }
+            else {
+                message.params = params;
+                message.result = result;
+                if (argv.responses) {
+                    logger.warn("nodeWebkitApi: Server Response\n%s", util.inspect(message, false, null, true));
+                }
+                return callback(null, result);
+            }
+        };
+
+        var _invokeControllerAction = function () {
+            var controller = require('./' + controllerName + '.js')(logger, argv);
+            if (controller) {
+                if (controller[actionName]) {
+                    var host = credentials.host;
+                    var user = credentials.user;
+                    var password = credentials.password;
+                    if (controller.allowsAnonymous || cbWrapper.credentialsComplete(host, user, password)) {
+                        cbWrapper.initialize(logger, host, user, password);
+                        // perform validate if defined inside controller
+                        _validate(controller, params, function (error) {
+                            if (error) {
+                                _logAndSendErrorOrResult(error, null);
+                            }
+                            else {
+                                // perform the action
+                                var client = cbWrapper;
+                                controller[actionName](client, params, function (error, result) {
+                                    if (error) {
+                                        _logAndSendErrorOrResult(error, null);
+                                    }
+                                    else {
+                                        result = result || {};
+                                        _logAndSendErrorOrResult(null, result);
+                                    }
+                                });
+                            }
+                        });
+                    }
+                    else {
+                        _logAndSendErrorOrResult('Missing host, user, or password in request [' + req.path + ']', null);
+                    }
+                }
+                else {
+                    _logAndSendErrorOrResult('Cannot find action [' + actionName + '] in controller [' + controllerName + '] from request path [' + req.path + ']', null);
+                }
+            }
+            else {
+                _logAndSendErrorOrResult('Cannot find controller [' + controllerName + '] from request path [' + req.path + ']', null);
+            }
+        };
+
+        _invokeControllerAction();
+    };
+})();
---
+++ app/nwApp.js
@@ -0,0 +1,165 @@
+'use strict';
+
+var app = angular.module('CBbread', [
+    'ui.router',
+    'ui.bootstrap',
+    'ngGrid'
+]);
+
+var util = require('util');
+
+var log4js = require('log4js');
+// https://github.com/nomiddlename/log4js-node/issues/112
+log4js.configure({ appenders: [ { type: "console", layout: { type: "basic" } } ], replaceConsole: true });
+var logger = log4js.getLogger('cb-bread');
+
+//logger.info("app = %s", util.inspect(app));
+//
+//var gui = require('nw.gui');
+//logger.info("gui = %s", util.inspect(gui));
+
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('dashboard', {
+        url: '/',
+        templateUrl: './views/dashboard.html',
+        controller: 'DashboardCtrl'
+    });
+
+    $stateProvider.state('console', {
+        url: '/console',
+        templateUrl: './views/console.html',
+        controller: 'ConsoleCtrl'
+    });
+
+    $stateProvider.state('credits', {
+        url: '/credits',
+        templateUrl: './views/credits.html',
+        controller: 'CreditsCtrl'
+    });
+
+    $stateProvider.state('database', {
+        url: '/databases',
+        templateUrl: './views/database/index.html',
+        controller: 'DatabaseCtrl'
+    });
+
+    $stateProvider.state('collection', {
+        url: '/collections/?did',
+        templateUrl: './views/collection/index.html',
+        controller: 'CollectionCtrl'
+    });
+
+    $stateProvider.state('document', {
+        url: '/documents/?did&cid',
+        templateUrl: './views/document/index.html',
+        controller: 'DocumentCtrl'
+    });
+}]);
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push(function ($q, $rootScope) {
+        return {
+            'request': function (config) {
+                $rootScope.$broadcast('loading-started');
+                return config || $q.when(config);
+            },
+            'response': function(response) {
+                $rootScope.$broadcast('loading-complete');
+                return response || $q.when(response);
+            }
+        };
+    });
+});
+
+app.directive("loadingIndicator", function () {
+    return {
+        restrict : "A",
+        link : function (scope, element) {
+            scope.$on("loading-started", function () {
+                element.css({"display" : ""});
+            });
+
+            scope.$on("loading-complete", function () {
+                element.css({"display" : "none"});
+            });
+        }
+    };
+});
+
+app.value('$', $);
+app.value('$alert', alert);
+
+app.factory('serverConfig', function ($alert, api) {
+    var controllerName = 'serverdata';
+
+    return {
+        serverConfig: null,
+        getConfig: function(callback) {
+            var self = this;
+            if (this.serverConfig) {
+                return callback(null, this.serverConfig);
+            } else {
+                api.request(controllerName, 'config', null, function (error, config) {
+                    if (error) {
+                        $alert(JSON.stringify(error, null, 2));
+                        return callback(error);
+                    }
+                    else {
+                        self.serverConfig = config;
+                        return callback(null, config);
+                    }
+                });
+            }
+        }
+    };
+});
+
+app.factory('credentials', function () {
+    return {
+        host: '',
+        user: '',
+        password: '',
+        set: function (host, user, password) {
+            this.host = host;
+            this.user = user;
+            this.password = password;
+        },
+        reset: function () {
+            this.host = '';
+            this.user = '';
+            this.password = '';
+        },
+        isConfigured: function () {
+            return (this.host && this.host.length > 0) ||
+                (this.user && this.user.length > 0) ||
+                (this.password && this.password.length > 0);
+        },
+        isConnected: function () {
+            return (this.host && this.host.length > 0) &&
+                (this.user && this.user.length > 0) &&
+                (this.password && this.password.length > 0);
+        }
+    };
+});
+
+var nodeWebkitApi = require('../api/nodeWebKitApi');
+var argv = { responses: true };
+
+app.factory('api', function ($http, credentials) {
+    return {
+        path: '/api',
+        request: function (controllerName, actionName, params, callback) {
+            nodeWebkitApi.invokeControllerAction(logger, argv, credentials, controllerName, actionName, params, function(err, data) {
+                if (err) {
+                    logger.error("nodeWebkitApi.invokeControllerAction %s.%s returned error: %s", controllerName, actionName, util.inspect(err));
+                    return callback(err);
+                } else {
+                    logger.debug("nodeWebkitApi.invokeControllerAction %s.%s returned data: %s", controllerName, actionName, util.inspect(data));
+                    return callback(null, data);
+                }
+            });
+        }
+    };
+});
